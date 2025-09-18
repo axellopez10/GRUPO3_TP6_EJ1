129aba5c0490313c5dc3c89934fe9c308580ac1b
Kamil Jofré <kamiljelias@gmail.com>
2025-09-18 18:51:53 -0300

    1-KAMIL
    -Quite clase pruebas
    -Agregue clase Producto
    -Agregue JFrame App

---
+++ src/grupo3_tp6_ej1/Producto.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grupo3_tp6_ej1;
+
+/**
+ *
+ * @author kamil
+ */
+public class Producto {
+    private String nombre;
+    private String categoria;
+    private double precio;
+    
+    public Producto(String nombre, String categoria, double precio){
+        this.nombre=nombre;
+        this.categoria=categoria;
+        this.precio=precio;
+    }
+    
+    public String getNombre(){return nombre;}
+    public String getCategoria(){return categoria;}
+    public double getPrecio(){return precio;}
+}
